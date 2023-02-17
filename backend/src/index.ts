86381beb75c1438dd9791c11fe9fa6b30129bc17
SanteriSuomi <54308700+SanteriSuomi@users.noreply.github.com>
2023-02-18 01:10:59 +0200

    Initialize basic express + socket.io server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,23 @@
-console.log("TEST!");
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, { serveClient: false })
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("No content")
+});
+
+io.on('connection', (socket) => {
+    socket.on("join", (msg) => {
+        console.log(msg)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
