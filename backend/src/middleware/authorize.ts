a7929c4bafa0555afd8044a2c1b01d1966d94c32
SanteriSuomi <54308700+SanteriSuomi@users.noreply.github.com>
2023-03-06 23:52:26 +0200

    Update authorize.ts
    Remove redundant semicolon

--- backend/src/middleware/authorize.ts
+++ backend/src/middleware/authorize.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 
 function authorize(req: Request, res: Response, next: NextFunction) {
